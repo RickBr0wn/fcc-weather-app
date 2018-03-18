2827baa58e53e9686d08b7e005d5f36d4fa018bc
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-18 21:38:24 +0000

    Completed date & time

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
     let country;
 
     $.getJSON("https://ipinfo.io/json", function (locationData) {
+        let day = getDay();
+        let date = getDateNumber() + " " + getMonthName();
+        startTime();
+
+        $('#day').html(day);
+        $('#date').html(date);
+
+
+
         console.log(locationData);
         coordsAPI = locationData.loc;
         country = locationData.country;
@@ -48,7 +57,9 @@ $(document).ready(function () {
                 $('#weatherTemp').html(weatherTemp + fahDegree);
             };
 
-            let icons = new Skycons({ "color": "white" });
+            let icons = new Skycons({
+                "color": "white"
+            });
 
             switch (iconAPI) {
                 case "clear-day":
@@ -108,3 +119,68 @@ $(document).ready(function () {
         });
     });
 });
+
+function getDay() {
+    var dateFull = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var dayName = weekday[dateFull.getDay()];
+
+    return dayName;
+}
+
+function getDateNumber() {
+    var dateFull = new Date();
+    var date = dateFull.getDate();
+
+    return date;
+}
+
+function getMonthName() {
+    var dateFull = new Date();
+    var months = new Array(12);
+    months[0] = "January";
+    months[1] = "February";
+    months[2] = "March";
+    months[3] = "April";
+    months[4] = "May";
+    months[5] = "June";
+    months[6] = "July";
+    months[7] = "August";
+    months[8] = "September";
+    months[9] = "October";
+    months[10] = "November";
+    months[11] = "December";
+
+    var month = months[dateFull.getMonth()];
+
+    return month;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
